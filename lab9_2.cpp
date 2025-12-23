000880cc3db6d2254b0ed0af4b0de32ce71a0cad
Peerawish <paepeerawishmakheaw@gmail.com>
2025-12-23 12:40:25 +0700

    23/12/2568

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,6 +3,20 @@ using namespace std;
 
 //Write the function printO() here
 
+void printO(int N, int M) {
+    if (N <= 0 || M <= 0) {
+        cout << "Invalid input";
+        return;
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cout << "O";
+        }
+        cout << endl;
+    }
+}
+
 int main(){
 	
 	printO(2,2);
@@ -22,3 +36,4 @@ int main(){
 	
 	return 0;
 }
+
